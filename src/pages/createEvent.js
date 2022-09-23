3fcad24477c6f056851c5e39a8d82c9bde9d8185
Myat Thura Soe <89846206+MyatThuraSoe@users.noreply.github.com>
2022-09-23 23:03:21 +0630

    Add files via upload

--- src/pages/createEvent.js
+++ src/pages/createEvent.js
@@ -1,11 +1,56 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { useRef } from 'react';
+import { useNavigate  } from 'react-router-dom';
+import '../assets/style/createEvent.css';
+
 function CreateEvent() {
+    const eventname = useRef();
+    const hostname = useRef();
+    const starttime = useRef();
+    const endtime = useRef();
+    const location = useRef();
+    const navigate = useNavigate();
+    function createAndNext() {
+        navigate("/event", {
+            state: {
+                eventname: eventname.current.value,
+                hostname: hostname.current.value,
+                starttime: starttime.current.value,
+                endtime: endtime.current.value,
+                location: location.current.value
+            },
+        }
+        );
+    }
     return(
-        <div>
-            <Link to='/event'>
-                <button>Event Page</button>
-            </Link>
+        <div className="create-event-container">
+            <div className="create-event">
+                <h2>Enter information about the Birthday Event</h2>
+                <div className='birthday-event-form'>
+                    <div>
+                        <label htmlFor="eventName">Event Name</label>
+                        <input id='eventName' className="eventName" name='eventName' type='text' ref={eventname}/>
+                    </div>
+                    <div>
+                        <label htmlFor="hostName">Host Name</label>
+                        <input id='hostName' className="hostName" name='hostName' type='text' ref={hostname}/>
+                    </div>
+                    <div>
+                        <label htmlFor="startTime">Start Time</label>
+                        <input id='startTime' className="startTime" name='startTime' type='text' placeholder="Eg. 18 August 2022, 6:00 PM" ref={starttime}/>
+                    </div>
+                    <div>
+                        <label htmlFor="endTime">End Time</label>
+                        <input id='endTime' className="endTime" name='endTime' type='text' placeholder="Eg. 19 August 2022, 10:30 AM" ref={endtime}/>
+                    </div>
+                    <div>
+                        <label htmlFor="location">Location</label>
+                        <input id='location' className="location" name='location' type='text' placeholder="Eg. Times Square Brm, New York City, New York" ref={location}/>
+                    </div>
+                    
+                    <button onClick={createAndNext}>Next</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/eventPage.js
+++ src/pages/eventPage.js
@@ -1,9 +1,13 @@
 import React from "react";
 import BirthdayCake from '../assets/img/BirthdayCake.png';
-
+import { useLocation, useResolvedPath } from "react-router-dom";
 import '../assets/style/eventPage.css';
-
+import { GoCalendar } from 'react-icons/go';
+import { MdLocationOn } from "react-icons/md";
 function EventPage(props) {
+    const location = useLocation();
+    var place = location.state.location.split(',');
+    var placeLocation = location.state.location.replace(place[0] + ',','');
     return(
         <div className="event-container">
             <div className="event">
@@ -11,9 +15,28 @@ function EventPage(props) {
                     <img src={BirthdayCake} className="birthdaycake-image" alt="birthday cake image"/>
                 </div>
                 <div className="birthday-event-data-container">
-                    <h2>Birthday Bash</h2>
-                    <span>Hosted by {props.name || 'Elysia'}</span>
+                    <h2>{location.state.eventname || 'Elysia birthday party'}</h2>
+                    Hosted by <span>{location.state.hostname || 'Elysia'}</span>
+                    <div className="event-data-row">
+                        <div className="icon-container">
+                            <GoCalendar/>
+                        </div>
+                        <div className="event-text-container">
+                            <span>{ location.state.starttime }</span><br/>
+                            To: <span> { location.state.starttime } </span>
+                        </div>
+                    </div>
+                    <div className="event-data-row">
+                        <div className="icon-container">
+                            <MdLocationOn/>
+                        </div>
+                        <div className="event-text-container">
+                            <span>{place[0]}</span><br/>
+                            <span>{ placeLocation }</span>
+                        </div>
+                    </div>
                 </div>
+                
             </div>
         </div>
     );
